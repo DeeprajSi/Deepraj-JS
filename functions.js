660ff7de6ae2751cf1bc7924ffce29a059d3eed4
Deepraj Singh <112300638+DeeprajSi@users.noreply.github.com>
2025-09-25 12:27:31 +0530

    Update functions.js
    Here we will study function deeply

--- functions.js
+++ functions.js
@@ -1,1 +1,12 @@
+//Function Declarations
+
+function func(){
+console.log('this is function declarations');
+}
+func();
+
+//Function Expression
+const funct = function(){
+  console.log('this is functio expression');
+}func();
 
